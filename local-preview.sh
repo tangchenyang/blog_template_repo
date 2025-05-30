98337665fdfc0e803d5666b504de0a691ad9e543
Chenyang Tang <44256449+tangchenyang@users.noreply.github.com>
2025-05-30 12:17:26 +0800

    Update local-preview.sh

--- local-preview.sh
+++ local-preview.sh
@@ -4,25 +4,32 @@ PROJECT_DIR="$CURRENT_SCRIPT_DIR"
 
 # clean hexo home
 HEXO_HOME="$PROJECT_DIR"/hexo-home
-if [ -d "${HEXO_HOME}" ]; then
+if [ "$1" == "-c" ]; then
   rm -rf "${HEXO_HOME}"
+  echo "****** Cleaned Hexo home..."
 fi
 
 # fix sed issue on MacOS
-brew install gnu-sed
+echo "****** Fixing sed issue on MacOS..."
+which gsed > /dev/null || ( echo "****** Installing gnu-sed..." && brew install gnu-sed )
 sed() {
   gsed "$@"
 }
 export -f sed
 
 # hexo setup
-export ENABLE_WEB_STATISTICS="false"
-export ENABLE_COMMENTS="false"
-export HEXO_LEANCLOUD_APP_ID=""
-export HEXO_LEANCLOUD_APP_KEY=""
-export HEXO_LEANCLOUD_APP_SERVER_URL=""
+if [ ! -d "${HEXO_HOME}" ]; then
+  echo "****** Setting up Hexo home..."
 
-bash scripts/hexo-setup.sh
+  export ENABLE_WEB_STATISTICS="false"
+  export ENABLE_COMMENTS="false"
+  export HEXO_LEANCLOUD_APP_ID=""
+  export HEXO_LEANCLOUD_APP_KEY=""
+  export HEXO_LEANCLOUD_APP_SERVER_URL=""
+
+  bash scripts/hexo-setup.sh
+fi
 
 # hexo start local server
+echo "****** Starting local server..."
 bash scripts/hexo-local-server.sh
